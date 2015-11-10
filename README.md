6f81b411a138a5f6d6aef0464e729a2bb5853899
CHeinrich <cheinrich@silversurfer7.de>
2015-11-10 12:13:59 +0100

    Symfony Project added

---
+++ README.md
@@ -0,0 +1,4 @@
+osp
+===
+
+A Symfony project created on November 9, 2015, 3:24 pm.
---
+++ src/EducationCalendarBundle/EducationCalendarBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EducationCalendarBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EducationCalendarBundle extends Bundle
+{
+}
