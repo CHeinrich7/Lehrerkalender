606712a0b1477a477366b4bfd55a3455479d4001
CHeinrich <cheinrich@silversurfer7.de>
2015-11-10 14:48:19 +0100

    base template added

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -1,13 +1,41 @@
+<?php
+use Symfony\Component\Templating\Helper\SlotsHelper;
+/* @var $view Symfony\Bundle\FrameworkBundle\Templating\TimedPhpEngine  */
+$slotsHelper = $view['slots']; /* @var $slotsHelper SlotsHelper */
+?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>{% block title %}Welcome!{% endblock %}</title>
-        {% block stylesheets %}{% endblock %}
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
-    </head>
-    <body>
-        {% block body %}{% endblock %}
-        {% block javascripts %}{% endblock %}
-    </body>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title><?php $slotsHelper->output('title') ?> - Lehrerkalender</title>
+
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/helper.css" rel="stylesheet">
+    <?php $slotsHelper->output('styles', ''); ?>
+
+    <script type="text/javascript"></script>
+</head>
+<body>
+<div class="container" id="header">
+    <div class="page-header">
+        <?php $slotsHelper->output('header', ''); ?>
+    </div>
+</div>
+
+<?php $slotsHelper->output('content', ''); ?>
+
+<?php $slotsHelper->output('footer', ''); ?>
+
+<script src="/js/jquery.js"></script>
+<script src="/js/bootstrap.js"></script>
+
+<script type="text/javascript">
+    $( document ).ready(function() {
+        <?php $slotsHelper->output('jQuery', ''); ?>
+    });
+</script>
+</body>
 </html>
