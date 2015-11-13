3a6bf98bcb95193c996ebe1e040b758dc6cba4f7
CHeinrich <cheinrich@silversurfer7.de>
2015-11-13 19:22:03 +0100

    markup and js in EducationCalendar changed

--- src/EducationCalendarBundle/Resources/views/Default/index.html.php
+++ src/EducationCalendarBundle/Resources/views/Default/index.html.php
@@ -19,124 +19,192 @@ $view->extend('::base.html.php');
 ?>
 
 <?php $slotsHelper->start('content'); ?>
-    <style>
-        .date {
-            transform:  rotate(-45deg);
-            margin-top: 20px;
-            width:      30px;
-            height:     20px;
-            margin-left:10px;
-        }
+<style>
+    .date {
+        transform:  rotate(-45deg);
+        margin-top: 20px;
+        width:      20px;
+        height:     20px;
+        margin-left:10px;
+    }
 
-        .inline {
-            display:        inline-block;
-            vertical-align: top;
-        }
+    .inline {
+        display:        inline-block;
+        vertical-align: top;
+    }
 
-        .inline table thead {
-            height:60px;
-        }
+    .inline table thead {
+        height: 60px;
+    }
 
-        .inline table tbody td {
-            height: 53px;
-            vertical-align: middle;
-        }
+    .inline table tbody td {
+        height: 40px;
+        vertical-align: middle;
+    }
 
-        .inline td label {
-            margin: 0;
-        }
+    .inline td label {
+        margin: 0;
+    }
 
-        .no-wrap {
-            white-space: nowrap;
-        }
+    .no-wrap {
+        white-space: nowrap;
+    }
 
-        th {
-            position:relative;
-        }
+    #table-marks td {
+        padding:    0;
+        text-align: center;
+        position:   relative;
+    }
 
-        .arrow {
-            position:absolute;
-            right: 0;
-            bottom: -5px;
-        }
+    #table-marks td > span {
+        width:  100%;
+        padding:9px 0;
+        display: inline-block;
+    }
 
-        #table-students {
-            position:absolute;
-            background: white;
-            z-index:2;
-            left:15px;
-        }
+    #table-students-holder {
+        position:relative;
+        width:150px;
+    }
 
-        #table-marks {
-            z-index:1;
-        }
+    .marked {
+        background-color: #eee;
+    }
 
-        #table-students-holder {
-            position:relative;
-            width:150px;
-            margin-left:-15px;
-        }
+    .special {
+        background-color: rgba(51, 204, 119, 0.5);
+    }
 
-        .marked {
-            background-color: #eee !important;
-        }
+    .exercise {
+        background-color: rgba(201, 48, 44, 0.5);
+    }
 
-        .ajax {
-            background-color: #5cb85c !important;
-        }
-    </style>
+    .ajax {
+        background-color: #5cb85c;
+    }
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12 no-wrap">
-                <div id="table-students-holder" class="inline">
-                    <table id="table-students" class="table table-striped">
-                        <thead>
+    #table-marks td:hover {
+        background-color: #b4b4b4;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+
+    #table-marks td span:hover * {
+        cursor: auto;
+    }
+
+    #mark-form-wrapper {
+        position: absolute;
+        border-color: #66afe9;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px rgba(102, 175, 233, 0.6);
+        outline: 0 none;
+        bottom:-200px;
+        display:none;
+        z-index:3;
+    }
+
+    #mark-form-wrapper .form-group:last-child {
+        margin-bottom: 0;
+    }
+
+    #mark-form-wrapper .btn {
+        min-width: 75px;
+    }
+
+    #mark-form-wrapper::before {
+        -webkit-box-sizing: content-box;
+        -moz-box-sizing: content-box;
+        box-sizing: content-box;
+        width: 0;
+        height: 0;
+        position: absolute;
+        content: "";
+        left: 8px;
+        top: -15px;
+        border: 13px solid rgba(0,0,0,0);
+        border-bottom: 15px solid #ccc;
+        border-top: 0 solid;
+    }
+</style>
+
+
+<div id="mark-form-wrapper" style="width:250px;" class="well">
+    <form action="" class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label col-sm-3">Note</label>
+            <div class="col-sm-9">
+                <input class="form-control text-center" type="text" value="100%" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-3">Typ</label>
+            <div class="col-sm-9">
+                <select name="" id="" class="form-control text-center">
+                    <option value="">Mündlich</option>
+                    <option value="">Sonderleistung</option>
+                    <option value="">Schriftlich</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-12">
+                <button type="button" data-abort class="btn btn-danger pull-left">
+                    <span class="glyphicon glyphicon-remove"></span>
+                </button>
+                <button type="button" data-submit class="btn btn-primary pull-right">
+                    <span class="glyphicon glyphicon-ok"></span>
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 no-wrap">
+            <div id="table-students-holder" class="inline">
+                <table id="table-students" class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php for($k = 0; $k < 20; $k++): ?>
                         <tr>
-                            <th>&nbsp;</th>
+                            <td>
+                                <label data-id="<?php echo $k; ?>" class="control-label student">Schüler Name</label>
+                            </td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        <?php for($k = 0; $k < 20; $k++): ?>
-                            <tr>
-                                <td>
-                                    <label data-id="<?php echo $k; ?>" class="control-label student">Schüler Name</label>
-                                </td>
-                            </tr>
+                    <?php endfor; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="inline">
+                <table id="table-marks" class="table table-striped">
+                    <thead>
+                    <tr>
+                        <?php $max = 20; for($i = 0; $i < $max; $i++): ?>
+                            <th>
+                                <div class="date" data-id="<?php echo $i; ?>">00.00.00</div>
+                            </th>
                         <?php endfor; ?>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="inline">
-                    <table id="table-marks" class="table table-striped">
-                        <thead>
-                            <tr>
-                                <?php $max = 80; for($i = 0; $i < $max; $i++): ?>
-                                <th>
-                                    <div class="date" data-id="<?php echo $i; ?>">00.00.00</div>
-                                    <div class="arrow">
-                                        <span class="glyphicon glyphicon-chevron-down"></span>
-                                    </div>
-                                </th>
-                                <?php endfor; ?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php for($k = 0; $k < 20; $k++): ?>
-                            <tr>
-                                <?php for($i = 0; $i < $max; $i++): ?>
-                                    <td>
-                                        <input data-id="" data-last-value="" onfocus="this.select();" type="text" class="form-control no-padding-horizontal text-center" autocomplete="off" />
-                                    </td>
-                                <?php endfor; ?>
-                            </tr>
-                        <?php endfor; ?>
-                        </tbody>
-                    </table>
-                </div>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php for($k = 0; $k < 20; $k++): ?>
+                        <tr>
+                            <?php for($i = 0; $i < $max; $i++): ?>
+                                <td><span>100%</span></td>
+                            <?php endfor; ?>
+                        </tr>
+                    <?php endfor; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
 <?php $slotsHelper->stop(); ?>
 
 
@@ -150,9 +218,11 @@ $view->extend('::base.html.php');
 
         tableStudentsId:'#table-students',
         tableMarksId:   '#table-marks',
+        formWrapperId:  '#mark-form-wrapper',
         tableStudents:  null,
         tableMarks:     null,
-        inputs:         null,
+        formWrapper:    null,
+        values:         null,
 
         current: {
             date:    null,
@@ -162,7 +232,8 @@ $view->extend('::base.html.php');
 
         log: function()
         {
-            if(this.debug !== true || arguments.length <= 0) {
+            var that = this;
+            if(that.debug !== true || arguments.length <= 0) {
                 return;
             }
 
@@ -172,6 +243,8 @@ $view->extend('::base.html.php');
                     console.log(arguments[index]);
                 }
             }
+
+            return that;
         },
 
         init: function()
@@ -180,56 +253,69 @@ $view->extend('::base.html.php');
 
             that.tableStudents = $(that.tableStudentsId);
             that.tableMarks =    $(that.tableMarksId);
-            that.inputs =        that.tableMarks.find('input');
+            that.values =         that.tableMarks.find('td');
+            that.formWrapper =   $(that.formWrapperId);
 
-            that.inputs
-                .on('focus focusout', {marks: that}, that.updateInput);
+            that.values
+                .on('click', {marks: that}, that.loadMarkForm)
+                .on('mouseenter', {marks: that}, this.setColors);
 
-            $(document).on('mouseenter', that.tableMarksId + ' td', {marks: that}, this.setColors);
-
-            $(window).on('scroll', function() {
-                that.tableStudents.css({
-                    'left': $(this).scrollLeft()
-                });
-            }).trigger('scroll');
+            that.setFormEvents();
         },
 
-        updateInput: function(event)
+        hideForm: function(event)
         {
-            var that    = event.data.marks,
+            var that = event.data.marks;
 
-                currVal = $(this).val(),
-                lastVal = $(this).data('last-value');
+            that.formWrapper.fadeOut();
+        },
 
-            if(event.type === 'focus')
-            {
-                $(this).data('last-value', currVal);
-            }
-            else if(event.type === 'focusout')
-            {
-                that.log('currVal = ' + currVal + ', lastVal = ' + lastVal);
+        setFormEvents: function()
+        {
+            var that = this;
+            that.formWrapper.find('[data-abort]').on('click', {marks: that}, that.hideForm);
+            that.formWrapper.on('click', function(event) { event.stopImmediatePropagation(); });
+        },
 
-                if(currVal !== lastVal) {
-                    that.updateCurrent($(this));
-                    that.sendAjax();
-                }
+        loadMarkForm: function(event)
+        {
+            var that    = event.data.marks;
 
-                that.resetCurrent();
-            }
+            that
+                .updateCurrent($(this))
+                .prepareAndLoadForm($(this));
+        },
+
+        prepareAndLoadForm: function($elm)
+        {
+            var that = this,
+
+                $form = that.formWrapper.find('form');
+
+            $elm.append(that.formWrapper);
+            that.formWrapper.fadeIn();
+
+            return that;
         },
 
         sendAjax: function()
         {
             var that = this;
+
+            return that;
         },
 
         resetCurrent: function()
         {
-            this.current = {
+            var that = this;
+
+            that.current = {
                 date:    null,
                 input:   null,
                 student: null
             };
+
+            return that;
         },
 
         updateCurrent: function($elm)
@@ -243,6 +329,8 @@ $view->extend('::base.html.php');
             that.current.student = that.tableStudents.find('.student').eq(position.y);
 
             that.log(that.current);
+
+            return that;
         },
 
         getPosition: function(elm)
@@ -264,7 +352,7 @@ $view->extend('::base.html.php');
 
                 position  = that.getPosition(this, that),
 
-                ySelector = 'tbody tr:nth-of-type('+ (position.y+1) +')',
+                ySelector = 'tbody tr:nth-of-type('+ (position.y+1) +') td',
                 xSelector = 'td:nth-of-type('+ (position.x+1) +')';
 
             $('.marked').removeClass('marked');
