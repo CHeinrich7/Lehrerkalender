1f9dfd6f774fa7851c70dde4101a3cdab1abc858
DanielFuss <danielfuss94@aol.com>
2015-11-16 14:25:46 +0100

    select changed

--- src/EducationCalendarBundle/Resources/views/Default/select.html.php
+++ src/EducationCalendarBundle/Resources/views/Default/select.html.php
@@ -15,78 +15,129 @@ $view->extend('::base.html.php');
 ?>
 
 <?php $slotsHelper->start('title'); ?>
-    Klasse auswaehlen
+Klasse auswaehlen
 <?php $slotsHelper->stop(); ?>
 
 <?php $slotsHelper->start('header'); ?>
-    <div class="col-xs-6">
-        <a href="/in/calendar"><h1 class="text-center">Kalender</h1></a>
-    </div>
-    <div class="col-xs-6">
-        <a href="/in/select"><h1 class="text-center">Benotung</h1></a>
-    </div>
+<div class="col-xs-6">
+    <a href="/in/calendar"><h1 class="text-center">Kalender</h1></a>
+</div>
+<div class="col-xs-6">
+    <a href="/in/select"><h1 class="text-center">Benotung</h1></a>
+</div>
 <?php $slotsHelper->stop(); ?>
 
 <?php $slotsHelper->start('content'); ?>
 
 <style>
-    .margin-top-30 {
-        margin-top: 30px;
-    }
-    .expandable-panel {
-        width:100%;
-        position:relative;
-        min-height:50px;
-        overflow:auto;
-        margin-bottom: 20px;
-        border:1px solid #999;
-    }
-    .expandable-panel-heading {
-        width:100%;
-        cursor:pointer;
-        min-height:50px;
-        clear:both;
-        background-color:white;
-        position:relative;
-        border-bottom:1px solid black;
-    }
-    .expandable-panel-heading:hover {
-        color:#666;
-    }
-    .expandable-panel-heading h2 {
-        padding:14px 10px 9px 15px;
-        font-size:18px;
-        line-height:20px;
-    }
-    .expandable-panel-content {
-        padding: 0 0px 0 0px;
-        margin-top:-999px;
-    }
-    .expandable-panel-content p {
-        padding:4px 0 6px 0;
-    }
-    .expandable-panel-content p:first-child {
-        padding-top:10px;
-    }
-    .expandable-panel-content p:last-child {
-        padding-bottom:15px;
-    }
-    .icon-close-open {
-        width:20px;
-        height:20px;
-        position:absolute;
-        background-image:url(http://i.imgur.com/Ir4S1H7.png);
-        right:15px;
+    h2, p, ol, ul, li {
+        margin: 0px;
+        padding: 0px;
+        font-size: 13px;
+        font-family: Arial, Helvetica, sans-serif;
     }
 
+    #container {
+        width: 300px;
+        margin: auto;
+        margin-top: 100px;
+    }
+
+    .expandable-panel {
+        width: 100%;
+        position: relative;
+        min-height: 50px;
+        overflow: auto;
+        margin-bottom: 20px;
+        border: 1px solid #999;
+    }
+
+    .expandable-panel-heading {
+        width: 100%;
+        cursor: pointer;
+        min-height: 50px;
+        clear: both;
+        background-color: white;
+        position: relative;
+        border-bottom: 1px solid black;
+    }
+
+    .expandable-panel-heading:hover {
+        color: #666;
+    }
+
+    .expandable-panel-heading h2 {
+        padding: 14px 10px 9px 15px;
+        font-size: 18px;
+        line-height: 20px;
+    }
+
+    .expandable-panel-content {
+        padding: 0 0px 0 0px;
+        margin-top: -999px;
+    }
+
+    .expandable-panel-content p {
+        padding: 4px 0 6px 0;
+        margin-left: 10px;
+    }
+
+    .expandable-panel-content p:first-child {
+        padding-top: 10px;
+    }
+
+    .expandable-panel-content p:last-child {
+        padding-bottom: 15px;
+    }
+
+    .icon-close-open {
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        background-image: url(http://i.imgur.com/Ir4S1H7.png);
+        right: 15px;
+    }
+
+    .customTableStyle td {
+        text-align: center;
+    }
+
+    .customTableStyle {
+        margin-bottom: 0px;
+    }
+
+    .smallInput {
+        width: 50px;
+        border: none;
+    }
+
+    .smallTD {
+        padding: 5px 5px 5px 5px !important;
+        vertical-align: inherit !important;
+    }
+
+    .tableTextArea {
+        border: none;
+        margin: 0px;
+        height: 100%;
+        width: 100%;
+    }
+
+    .tableTextAreaTD {
+        padding: 5px 5px 5px 5px;
+    }
 </style>
+
+
 <div class="container">
     <div class="expandable-panel" id="cp-1">
         <div class="expandable-panel-heading">
-            Klasse hinzufügen<span class="icon-close-open"></span>
+            <h2> Klasse hinzufügen<span class="icon-close-open"></span></h2>
         </div>
         <div class="expandable-panel-content">
-
+                <p><input type="text" class="form-control" placeholder="Klasse" aria-describedby="basic-addon1"></p>
+                <p><input type="text" class="form-control" placeholder="Fach" aria-describedby="basic-addon1"></p>
+                <p><button type="button" class="btn btn-default">Anlegen</button></p>
         </div>
     </div>
 </div>
@@ -126,7 +177,6 @@ $view->extend('::base.html.php');
                 }
             }
         }
-
 
 
         $('.expandable-panel-heading').click(function () {
