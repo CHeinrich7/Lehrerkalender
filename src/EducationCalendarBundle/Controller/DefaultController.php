69a97123400b712500af78670cf6dd0d21000a66
CHeinrich <cheinrich@silversurfer7.de>
2015-11-16 15:37:03 +0100

    some little changes

--- src/EducationCalendarBundle/Controller/DefaultController.php
+++ src/EducationCalendarBundle/Controller/DefaultController.php
@@ -6,11 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('EducationCalendarBundle:Default:index.html.php', array('name' => $name));
-    }
-
     public function endAction()
     {
         var_dump($this->getUser()->getRoles()[0]);
