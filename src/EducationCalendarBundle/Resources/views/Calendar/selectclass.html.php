a5360e8cef05dcca4175796c5af1a94573498c5e
CHeinrich <cheinrich@silversurfer7.de>
2015-11-17 12:59:51 +0100

    data prepared for database connection

--- src/EducationCalendarBundle/Resources/views/Calendar/selectclass.html.php
+++ src/EducationCalendarBundle/Resources/views/Calendar/selectclass.html.php
@@ -99,77 +99,139 @@ Klasse auswaehlen
 <div class="container">
     <div class="panel-group accordion" id="accordion" role="tablist" aria-multiselectable="true">
         <?php
-        $datas = [
-            [
-                'title' => 'Klasse anlegen',
-                'content' => '
-
-<p><div class="container">
-    <div class="row">
-        <div class="col-xs-4">
-            <select name="MyFancyInput" id="MyFancyInput" class="form-control chosen" autocomplete="off">
-                <option value="1">Köln</option>
-                <option value="2">Kiel</option>
-                <option value="3">Düsseldorf</option>
-                <option value="4">Berlin</option>
-                <option value="5">München</option>
-                <option value="6">Stuttgart</option>
-                <option value="7">Hamburg</option>
-                <option value="8">Bremen</option>
-                <option value="9">Hürth</option>
-                <option value="10">Öskirschen</option>
-                <option value="11">Öskirschen</option>
-            </select>
-        </div>
-        <div class="col-xs-4">
-            <select name="MyFancyInput" id="MyFancyInput2" class="form-control chosen" autocomplete="off">
-                <option value="1">Köln</option>
-                <option value="2">Kiel</option>
-                <option value="3">Düsseldorf</option>
-                <option value="4">Berlin</option>
-                <option value="5">München</option>
-                <option value="6">Stuttgart</option>
-                <option value="7">Hamburg</option>
-                <option value="8">Bremen</option>
-                <option value="9">Hürth</option>
-                <option value="10">Öskirschen</option>
-                <option value="11">Öskirschen</option>
-            </select>
-        </div>
-        <div class="col-xs-4"><button type="button" class="btn btn-default">Anlegen</button></div>
-    </div></p>'
+        $allClasses = [
+            'title' => 'Klasse anlegen',
+            'content' => [
+                'classes' => [
+                    'FIA30',
+                    'FIA31',
+                    'FIA32',
+                    'FIA33',
+                    'FIA34',
+                    'FIA35',
+                    'FIA36'
+                ],
+                'subjects' => [
+                    'ITK',
+                    'ANW (WO)',
+                    'ANW (NL)',
+                    'Sport',
+                    'Religion',
+                    'Mathe',
+                    'Deu',
+                    'Eng'
+                ]
             ],
-
         ];
         ?>
-        <?php foreach ($datas as $key => $data): ?>
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="heading<?php echo $key; ?>">
-                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $key; ?>"
-                       aria-expanded="false" aria-controls="collapse<?php echo $key; ?>">
-                        <h4 class="panel-title"><?php echo $data['title']; ?></h4>
-                    </a>
-                </div>
-                <div id="collapse<?php echo $key; ?>" class="panel-collapse collapse" role="tabpanel"
-                     aria-labelledby="heading<?php echo $key; ?>">
-                    <div class="panel-body">
-                        <?php echo $data['content']; ?>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingNew>">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseNew"
+                   aria-expanded="false" aria-controls="collapseNew">
+                    <h4 class="panel-title">Klasse anlegen</h4>
+                </a>
+            </div>
+            <div id="collapseNew" class="panel-collapse collapse" role="tabpanel"
+                 aria-labelledby="headingNew">
+                <div class="panel-body">
+                    <div class="container">
+                        <div class="row">
+                            <form id="newClassForm" action="#" class="form-horizontal">
+                                <div class="col-xs-4">
+                                    <select id="educationClass" name="education_class" class="form-control chosen"
+                                            autocomplete="off">
+                                        <?php
+                                        foreach ($allClasses['content']['classes'] as $key => $class) {
+                                            echo sprintf('<option value="%s">%s</option>', $key, $class);
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-xs-4">
+                                    <select id="educationSubject" name="education_subject" class="form-control chosen"
+                                            autocomplete="off">
+                                        <?php
+                                        foreach ($allClasses['content']['subjects'] as $key => $subject) {
+                                            echo sprintf('<option value="%s">%s</option>', $key, $subject);
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-xs-4">
+                                    <button type="submit" class="btn btn-default">Anlegen</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="container margin-top-30">
+    <div class="list-group">
+        <?php
+        $educationClasses = [
+            [
+                'class' => 'FIA32',
+                'subject' => 'ITK'
+            ],
+            [
+                'class' => 'FIS33',
+                'subject' => 'ITK'
+            ],
+            [
+                'class' => 'FIA32',
+                'subject' => 'ANW'
+            ],
+        ]
+        ?>
+        <?php foreach ($educationClasses as $class): ?>
+            <a href="<?php echo $routerHelper->generate('mark_overview', ['subject' => 1]); ?>"
+               class="list-group-item">
+                <?php echo sprintf('%s - %s', $class['class'], $class['subject']); ?>
+            </a>
         <?php endforeach; ?>
     </div>
-
-
 </div>
-
-<div class="container margin-top-30" style="margin-left: -15px">
-    <div class="list-group">
-        <a href="#" class="list-group-item">FIA32 - ANW</a>
-        <a href="#" class="list-group-item">FIA32 - ITK</a>
-    </div>
-</div>
-
 
 <?php $slotsHelper->stop(); ?>
+
+<script>
+    <?php $slotsHelper->start('jQuery'); ?>
+        $('#newClassForm').submit(function()
+        {
+            var $edClassOption = $('#educationClass').find('option:selected'),
+                $subjectOption = $('#educationSubject').find('option:selected'),
+                data;
+
+            data = {
+                'education_class': {
+                    'key': $edClassOption.text(),
+                    'val': $edClassOption.val()
+                },
+                'education_subject': {
+                    'key': $subjectOption.text(),
+                    'val': $subjectOption.val()
+                }
+            };
+
+            $.ajax({
+                'url': '<?php echo $routerHelper->generate('subject_class_new'); ?>',
+                'data': data,
+                success: function(response) {
+                    if(response.success) {
+                        console.log(response.data);
+                    }
+                },
+                error: function(response) {
+                    alert(response.error)
+                }
+            });
+
+            return false;
+        });
+    <?php $slotsHelper->stop(); ?>
+</script>
 
