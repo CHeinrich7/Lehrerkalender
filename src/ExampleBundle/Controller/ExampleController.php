2ffb6f3a304c5b7aceca3f53cb6f3ec5758b700e
CHeinrich <cheinrich@silversurfer7.de>
2015-11-17 07:29:34 +0100

    uniform name conventions

---
+++ src/ExampleBundle/Controller/ExampleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ExampleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ExampleController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ExampleBundle:Examples:index.html.php', array('name' => $name));
+    }
+
+    public function calendarAction() {
+        return $this->render('ExampleBundle:Examples:calendar.html.php');
+    }
+}
+
+
+
+
+
