346beebe14b7425239f233587a1304051f02cfc2
CHeinrich <cheinrich@silversurfer7.de>
2015-11-17 11:20:50 +0100

    multiple chosen fix

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -63,31 +63,34 @@ $slotsHelper = $view['slots']; /* @var $slotsHelper SlotsHelper */
 </script>
 <script>
     (function(document, $) {
-        $( document ).ready(function() {
+
+        function chosenOptionalValue()
+        {
             var options = {
                     max_selected_options:       1,
                     disable_search_threshold:   10,
                     width:                      '100%'
                 },
-                $inputs = $('.chosen');
 
-            $inputs.each(function() {
-                $input = $(this);
+            $input = $(this);
 
-                $input.chosen(options)
-                    .parent()
-                    .on('keydown', function(event) {
-                        if(event.keyCode === 13) {
-                            var $defaultInput = $(this).find('input');
+            $input.chosen(options)
+                .parent()
+                .on('keydown', function(event) {
+                    if(event.keyCode === 13) {
+                        var $defaultInput = $(this).find('input');
 
-                            $input.find('.optional').remove();
+                        $input.find('.optional').remove();
 
-                            $input.append('<option value="" class="optional" selected="selected">'+ $defaultInput.val() +'</option>');
+                        $input.append('<option value="" class="optional" selected="selected">'+ $defaultInput.val() +'</option>');
 
-                            $input.trigger('chosen:updated');
-                        }
-                    });
-            });
+                        $input.trigger('chosen:updated');
+                    }
+                });
+        }
+
+        $( document ).ready(function() {
+            $('.chosen').each(chosenOptionalValue);
         });
     })(document, jQuery);
 </script>
