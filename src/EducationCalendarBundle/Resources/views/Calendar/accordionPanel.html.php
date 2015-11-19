446ca3ba3c3167f9781b9e81128429cb9d29f31a
CHeinrich <cheinrich@silversurfer7.de>
2015-11-19 18:25:38 +0100

    outsourcing calendar view parts

---
+++ src/EducationCalendarBundle/Resources/views/Calendar/accordionPanel.html.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @var $view           \Symfony\Bundle\FrameworkBundle\Templating\TimedPhpEngine
+ *
+ * @var $data           array
+ * @var $index          integer
+ * @var $subjects       array
+ */
+
+?>
+<div class="panel-content panel-content-<?php echo strtolower($data['day']); ?>">
+    <div class="panel-heading" role="tab">
+        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-<?php echo $index; ?>" aria-expanded="false" aria-controls="collapse-<?php echo $index; ?>">
+            <h4 class="panel-title"><?php echo $data['day']; ?></h4>
+        </a>
+    </div>
+    <div id="collapse-<?php echo $index; ?>" class="panel-collapse collapse" role="tabpanel">
+        <div class="panel-body">
+            <?php foreach($data['blocks'] as $block => $teachingUnit): ?>
+                <div class="col-xs-12 td-div td-div-<?php echo $block; ?>">
+                    <?php echo $view->render('EducationCalendarBundle:Calendar:accordionPanelForm.html.php', [
+                        'block'     => $block,
+                        'date'      => $data['day'],
+                        'subjects'  => $subjects,
+                        'teachingUnit' => $teachingUnit
+                    ]); ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
---
+++ src/EducationCalendarBundle/Resources/views/Calendar/accordionPanelForm.html.php
@@ -0,0 +1,72 @@
+<?php
+use \EducationCalendarBundle\Entity\TeachingUnit;
+/**
+ * @var $view           \Symfony\Bundle\FrameworkBundle\Templating\TimedPhpEngine
+ *
+ * @var $block          integer
+ * @var $date           string
+ * @var $data           array
+ * @var $teachingUnit   TeachingUnit|null
+ * @var $subjects       array
+ */
+
+$data = [
+    'date'      => $date,
+    'block'     => $block+1,
+    'room'      => '',
+    'subject_class' => [
+        'selected' => '',
+        'choices' => $subjects
+    ],
+    'content'   => '',
+    'notice'    => ''
+];
+
+if($teachingUnit instanceof TeachingUnit === true) {
+    $data['room']    = $teachingUnit->getRoom();
+    $data['content'] = $teachingUnit->getContent();
+    $data['notice']  = $teachingUnit->getNotice();
+    $data['subject_class']['selected'] = $teachingUnit->getSubject()->getId();
+}
+
+?>
+<form action="#" class="form-horizontal row" data-block="<?php echo $data['block']; ?>" data-date="<?php echo $data['date']; ?>" autocomplete="off">
+    <div class="col-lg-3 col-xs-4">
+        <div class="row">
+            <div class="col-lg-4 col-sm-3 cell">
+                <label>
+                    <input type="text" class="form-control" name="block" placeholder="<?php echo $data['block']; ?>" autocomplete="off" disabled />
+                </label>
+            </div>
+            <div class="col-lg-4 col-sm-3 cell">
+                <label>
+                    <input type="text" class="form-control" name="room" placeholder="Raum" value="<?php echo $data['room']; ?>" autocomplete="off" maxlength="5"/>
+                </label>
+            </div>
+            <div class="col-lg-4 col-sm-6 cell">
+                <label>
+                    <select class="form-control placeholder chosen-select" name="subject_class" autocomplete="off">
+                        <option value="" class="placeholder">Klasse - Fach</option>
+                        <?php foreach($data['subject_class']['choices'] as $index => $value): ?>
+                            <option
+                                value="<?php echo $index; ?>"
+                                <?php if($index == $data['subject_class']['selected']): ?>selected="selected"<?php endif; ?>>
+                                <?php echo $value; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-5 col-sm-4 col-xs-8 cell">
+        <label>
+            <textarea class="form-control height-34" name="content" placeholder="Inhalt" autocomplete="off"><?php echo $data['content']; ?></textarea>
+        </label>
+    </div>
+    <div class="col-lg-4 col-md-3 col-sm-4 col-xs-8 cell">
+        <label>
+            <textarea class="form-control height-34" name="notice" placeholder="Notiz" autocomplete="off"><?php echo $data['notice']; ?></textarea>
+        </label>
+    </div>
+</form>
