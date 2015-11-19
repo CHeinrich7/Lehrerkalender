9866331d948ab65c4bac987281ee74a3325443ad
CHeinrich <cheinrich@silversurfer7.de>
2015-11-19 18:23:02 +0100

    optional: filter subjects by user

--- src/SubjectBundle/Entity/Repository/SubjectRepository.php
+++ src/SubjectBundle/Entity/Repository/SubjectRepository.php
@@ -10,15 +10,27 @@ namespace SubjectBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
+use SubjectBundle\Entity\SubjectEntity;
+use UserBundle\Entity\User;
 
 class SubjectRepository extends EntityRepository
 {
-    public function findAllDistinct()
+    /**
+     * @param User $user
+     * @return SubjectEntity[]
+     */
+    public function findAllDistinct($user = null)
     {
         /** @var QueryBuilder $qb */
         $qb = $this->createQueryBuilder('s');
 
         $qb->distinct(true);
+
+        if($user instanceof User) {
+            $qb
+                ->where('s.createdBy = :user')
+                ->setParameter('user', $user);
+        }
 
         return $qb->getQuery()->getResult();
     }
